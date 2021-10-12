a687944b9ebccfe5ab95f84b919b0cd44e44d3c8
Amit <amitkr24031995@gmail.com>
2021-10-12 18:15:53 +0530

    Adding UC2ExceptionMoodAnalyzer

---
+++ UC1MoodAnilizer/UC2MoodAnalyzer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2ExceptionMoodAnalyzer
+{
+    public class UC2MoodAnalyzer
+    {
+        private string message;
+        public UC2MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyserMood()
+        {
+            try
+            {
+                if (this.message.Contains("Sad"))
+                {
+                    return "Sad";
+                }
+                else
+                {
+                    return "Happy";
+                }
+            }
+            catch
+            {
+                return "Happy";
+            }
+
+
+        }
+    }
+}
